532403fc060115868a715227d2d01cc2aca51562
Keldon Lopez <keldon.lopz@gmail.com>
2023-01-27 19:00:57 -0700

    Solved

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,12 @@
 // Write your solution below this line:
+for (let index = 1; index <= 50; index++) {
+    if (index % 5 === 0 && index % 3 === 0) {
+      console.log('fizzbuzz');
+    } else if (index % 3 === 0){
+      console.log('fizz');
+    } else if(index % 5 === 0){
+      console.log('buzz')
+    } else {
+      console.log(index);
+    }
+  }
